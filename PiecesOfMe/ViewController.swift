02886c8c2189b2f4edb8c065bc23ef86c7b6b3df
Hyunwook <hjhsha827@gmail.com>
2024-11-20 23:18:20 +0900

    init: initialize  project

---
+++ PiecesOfMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PiecesOfMe
+//
+//  Created by 이현욱 on 11/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PiecesOfMeUITests/PiecesOfMeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PiecesOfMeUITestsLaunchTests.swift
+//  PiecesOfMeUITests
+//
+//  Created by 이현욱 on 11/20/24.
+//
+
+import XCTest
+
+final class PiecesOfMeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
